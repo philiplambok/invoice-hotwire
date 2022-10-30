e70b07d9b7225805b6cbdb136385668a9d08476a
Philip Lambok <philiplambok71@gmail.com>
2022-10-30 11:46:41 +0700

    Finish setup the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+amounts = [100_000, 200_000, 300_000, 400_000, 500_000, 250_000]
+
+100.times do
+  Product.create!(name: Faker::Food.dish, amount: amounts.sample)
+  customer_name = Faker::Name.name
+  Customer.create!(name: customer_name, email: Faker::Internet.email(name: customer_name))
+end
