a9613dcb3868f398741ad250a5579c33a64c2522
Philip Lambok <philiplambok71@gmail.com>
2022-11-06 15:39:48 +0700

    Make the add new product link worked

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,12 +5,17 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.new
   end
 
-  def create
-    binding.pry
-  end
+  def create; end
 
   def products_options
     products = Product.all
     products.map { |product| [product.name, product.id, { data: { price_per_unit: product.amount } }] }
+  end
+
+  def add_product_item
+    respond_to do |format|
+      format.html { head :no_content }
+      format.turbo_stream
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :invoices, only: %i[new create]
+  resources :invoices, only: %i[new create] do
+    collection do
+      post :add_product_item
+    end
+  end
   root to: 'invoices#new'
 end
