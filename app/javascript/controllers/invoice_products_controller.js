a3da15116c6f9a36b1825ac6e28d3fafc1f9bde1
Philip Lambok <philiplambok71@gmail.com>
2022-11-06 12:31:17 +0700

    Add action triggered when choose another product

--- app/javascript/controllers/invoice_products_controller.js
+++ app/javascript/controllers/invoice_products_controller.js
@@ -2,11 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="invoice-products"
 export default class extends Controller {
-  static targets = ["productItem", "pricePerUnit", "total"]
+  static targets = ["unit", "productItem", "pricePerUnit", "total"]
 
   updateProductItemPrice(event) {
     const pricePerUnit = this.productItemTarget.selectedOptions[0].dataset.pricePerUnit
     this.pricePerUnitTarget.value = pricePerUnit
     this.totalTarget.value = pricePerUnit * event.target.value
+  }
+
+  clearPrices() {
+    this.unitTarget.value = ''
+    this.pricePerUnitTarget.value = ''
+    this.totalTarget.value = ''
   }
 }
