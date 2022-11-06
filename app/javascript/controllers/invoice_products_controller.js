a784a050566c770f3ca6c674601ea39cd4515497
Philip Lambok <philiplambok71@gmail.com>
2022-11-06 12:24:56 +0700

    Working on price stuff

--- app/javascript/controllers/invoice_products_controller.js
+++ app/javascript/controllers/invoice_products_controller.js
@@ -2,8 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="invoice-products"
 export default class extends Controller {
-  static targets = ["pricePerUnit", "total"]
+  static targets = ["productItem", "pricePerUnit", "total"]
 
   updateProductItemPrice(event) {
+    const pricePerUnit = this.productItemTarget.selectedOptions[0].dataset.pricePerUnit
+    this.pricePerUnitTarget.value = pricePerUnit
+    this.totalTarget.value = pricePerUnit * event.target.value
   }
 }
