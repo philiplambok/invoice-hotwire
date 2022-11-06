50f2a6e481354d96bcf1ced52d05185f8dd8a64b
Philip Lambok <philiplambok71@gmail.com>
2022-11-06 16:09:52 +0700

    do small refactoring

--- app/javascript/controllers/invoice_products_controller.js
+++ app/javascript/controllers/invoice_products_controller.js
@@ -5,19 +5,26 @@ export default class extends Controller {
   static targets = ["unit", "productItem", "pricePerUnit", "total"]
 
   updateProductItemPrice(event) {
-    const pricePerUnit = this.productItemTarget.selectedOptions[0].dataset.pricePerUnit
-    this.pricePerUnitTarget.value = this.toIdr(pricePerUnit)
+    const pricePerUnit = this.findPricePerUnitFor(this.productItemTarget)
     const total = pricePerUnit * event.target.value
     this.totalTarget.value = this.toIdr(total)
     this.totalTarget.setAttribute('data-amount', total)
     this.totalTarget.dispatchEvent(new Event('change'))
   }
 
-  clearPrices() {
-    this.unitTarget.value = ''
-    this.pricePerUnitTarget.value = ''
-    this.totalTarget.value = ''
+  updatePrices(event) {
+    if(event.target.selectedOptions[0].value == '') {
+      this.unitTarget.value = ''
+      this.pricePerUnitTarget.value = ''
+      this.totalTarget.value = ''
+    } else {
+      this.pricePerUnitTarget.value = this.toIdr(this.findPricePerUnitFor(event.target))
+    }
     this.totalTarget.dispatchEvent(new Event('change'))
+  }
+
+  findPricePerUnitFor(element) {
+    return element.selectedOptions[0].dataset.pricePerUnit
   }
 
   toIdr(number) {
